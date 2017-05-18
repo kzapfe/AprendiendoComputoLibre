ecd8fb492674c8cc7704190e5e168a5eed3604fe
FMendezSlc <fmendezslc@gmail.com>
2017-05-17 23:55:36 -0500

    Add files via upload
    Los cambios funcionan pero solo para el primer estimulo. Agruegué if/else loops para que reconozca la pista 'PP' del nombre (key) y alije apropiadamente entre el primer y segundo estímulos, pero no jal. Volvemos a tener el mismo error de no encontrar nada o encontrar otros puntos.

--- Python/v_m module.py
+++ Python/v_m module.py
@@ -1,17 +1,14 @@
-
 ''' Colección de funciones para extracción de datos y ajuste de funciones para realizar un Variance-Mean Analysis '''
 
 # librerías y módulos requeridos
 from neo import io  # paquete diseñado para abrir archivos de propietario
 import quantities as qn
 import numpy as np
+import os
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sb
 %matplotlib inline
-import os
-
-
+import matplotlib.pyplot as plt
 ''' Funciones que nos ayudan a obtener datos de un
 archivo experimental'''
 
@@ -23,7 +20,12 @@ def get_data(recording):
     return data
 
 
-def find_stim(trace, threshold=100, PP=0):
+def set_baseline(trace, many=20):
+    result = trace - np.mean(trace[0:many - 1])
+    return result
+
+
+def find_stim(trace, threshold=70, PP=0):
     '''[trace=data.segments[x], num, num -> num]
     Regresa el índice del determinado(PP) punto que supere el umbral por defecto.'''
     stim = np.where(trace.analogsignals[0] > threshold)[0][PP]
@@ -36,15 +38,22 @@ def extract_event(trace, stim_index, start=50, stop=500):
     return extracted
 
 
-def block_events(data, threshold=100, start=50, stop=500, PP=0):
-    '''funcion que a partir de un Bloque de neo crea una lista de arrays de eventos recortados y alineados. No necesitamos crear un nuevo neo.Block, estos arrays conservan toda la información de todas formas.
- '''
+def block_events(data, threshold=70, start=50, stop=500, PP=0, many=100):
+    '''funcion que a partir de un Bloque de neo crea una lista de arrays de eventos recortados y alineados.
+    No necesitamos crear un nuevo neo.Block, estos arrays conservan toda la información de todas formas.
+  '''
     clean_data = []  # events of interest
     for trace in data.segments:
-        # Toma el primer canal (activo), de cada analogsignal y la recorta
-        stim_index = find_stim(trace, threshold, PP)
-        event = extract_event(trace, stim_index, start, stop)
-        clean_data.append(event)  # events of interest
+        bs_window = trace.analogsignals[0][0:many]
+        if np.std(bs_window) > 20.0:
+            print(' Analogsignal ' + str(k) + ' has something fishy')
+        else:
+            offset = np.mean(bs_window)
+            # Toma el primer canal (activo), de cada analogsignal y la recorta
+            trace.analogsignals[0] -= offset
+            stim_index = find_stim(trace, threshold, PP)
+            event = extract_event(trace, stim_index, start, stop)
+            clean_data.append(event)  # events of interest
     return clean_data
 
 
@@ -59,8 +68,7 @@ def get_peaks(clean_data):
     return peaks
 
 
-''' Hasta aquí todas funcionan individualmente. tidy_dic debería correr archivo por archivo extraer el nombre como llave y asignarle el array de picos. La extracción de nombres y asignación al diccionario funcionan pero por alguna razón las funciones de extracción de datos no funcionan en loop.
-    La idea final sería pasarle un directorio de un folder que sea en sí mismo un experimento completo y regrese un diccionario con los nombres de los factores como llaves y su respectivo array con los valores de minimos (picos). Este diccionario podría pasarse a un pandas.DataFrame fácilmente si esta bien ordenado.
+''' tidy_dic() funciona, calado y garantizado. Solo falta agregar un if/else si en el nombre va PP2 que tome el segundo estimulo, si no que tome el primero.
 '''
 
 
@@ -70,30 +78,35 @@ def tidy_dic(cell):
     cell_name = cell.split('/')[-2]
     recordings = [cell + rec for rec in os.listdir(cell)]
     tidy_cell = {}
+    k = 0
     for recording in recordings:
         key = (recording.split('/')[-1]).strip('_{}.abf'.format(cell_name))
+        print('voy en la llave ' + key + ' llevo ' + str(k))
+        k += 1
         data = get_data(recording)
-        clean_data = block_events(data, threshold=100, start=50, stop=500, PP=0)
-        peaks = get_peaks(clean_data)
-        tidy_cell[key] = peaks
+        if 'PP2' in key:
+            clean_data = block_events(data, PP=1)
+            peaks = get_peaks(clean_data)
+            tidy_cell[key] = peaks
+        else:
+            clean_data = block_events(data, PP=0)
+            peaks = get_peaks(clean_data)
+            tidy_cell[key] = peaks
     return tidy_cell
 
 
-tidy_dic(cell)
-cel
+def datatodf(tidydic):
+    ''' Convert dictionary of minimums to dataframe R style '''
+        result = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in tidydic.items()]))
+    return result
+
+
 cell = '/Users/felipeantoniomendezsalcido/Desktop/V_M analysis/C1_21_2_17/'
-recordings = [cell + rec for rec in os.listdir(cell)]
-recording = recordings[1]
-all_peaks = []
-for rec in recordings:
-    data = get_data(rec)
-    for trace in data.segments:
-        stim_index = find_stim(trace)
-        extracted = extract_event(trace, stim_index)
-        peaks = np.min(extracted)
-        all_peaks.append(peaks)
-return all_peaks
-data
-all_peaks
-for rec in recordings:
-    print(rec)
+
+cell_dict = tidy_dic(cell)
+cell_dict
+rec = '/Users/felipeantoniomendezsalcido/Desktop/V_M analysis/C1_21_2_17/PP2_H4hz_C1_21_2_17.abf'
+data = get_data(rec)
+
+for i in data.segments:
+    plt.plot(i.analogsignals[0])
