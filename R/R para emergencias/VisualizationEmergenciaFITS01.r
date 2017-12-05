8fb9593eb24386dc89c7ac6991aa6b79e9570dce
karel <kzapfe@gmail.com>
2017-12-05 16:59:40 -0600

    raster neuronas

--- R/R para emergencias/VisualizationEmergenciaFITS01.r
+++ R/R para emergencias/VisualizationEmergenciaFITS01.r
@@ -9,3 +9,4 @@ datos$datum <- mdy_hms(datos$Timestamp, tz="Mexico/General")
 datos$fecha <- date(datos$datum)
 
 grafo1 <- ggplot(datos, aes(date(datos$fecha),fill = datos$Tipo.de.Transporte)) + geom_bar()+scale_fill_manual(values=pal, na.value="black")
+
